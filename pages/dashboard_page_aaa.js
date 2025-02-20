6e39b3777ab9b5fab5fd7419218c0b014a0eb778
Yaomin Wang <areolokinstain@gmail.com>
2025-02-20 09:29:14 +0800

    Update dashboard_page_aaa.js

--- pages/dashboard_page_aaa.js
+++ pages/dashboard_page_aaa.js
@@ -709,7 +709,7 @@ export default function Home() {
                         </label>
 
                         <label className="flex flex-col text-sm font-medium text-gray-700">
-                            <span>Time Periods:</span>
+                            <span>Time Interval:</span>
                             <select
                                 className="border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 onChange={(e) =>
